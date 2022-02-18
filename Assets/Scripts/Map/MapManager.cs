2a77f7dd0dad736809b754c92ecde0924e6175fa
imjae <code@kakao.com>
2022-02-18 10:34:39 +0900

    MapGenerator 타일 초기화 로직 수정, 클래스 분할

--- Assets/Scripts/Map/MapManager.cs
+++ Assets/Scripts/Map/MapManager.cs
@@ -7,17 +7,16 @@ using UnityEditor;
 public class MapManager : Singleton<MapManager>
 {
     #region Fields
-    [Header("Map Settings")]
-    public int width;
-    public int height;
+    [HideInInspector] public int width;
+    [HideInInspector] public int height;
     private Tile[,] _tileArray;
 
     [Header("Tile References")]
     public TileManager tileManager;
-    public Tile tileResource;
     #endregion
 
     #region Properties
+    // 실제 맵의 타일을 담고있는 2차원 배열
     public Tile[,] TileArray
     {
         get => _tileArray;
@@ -35,25 +34,28 @@ public class MapManager : Singleton<MapManager>
         int dx = 0;
         int dy = 0;
 
+        // 맵의 높이 크기만큼 배열의 가로축 개수가 정해지는 방식
         TileArray = new Tile[height, width];
 
         int halfWidth = Mathf.RoundToInt(width * 0.5f);
         int halfHeight = Mathf.RoundToInt(height * 0.5f);
 
+        // 배열의 x축 y축은 실제 타일맵의 좌표 x, y와 반전된 상태로 생각해야 한다.
         for (int y = -halfHeight; y < halfHeight; y++)
         {
             for (int x = -halfWidth; x < halfWidth; x++)
             {
-                Vector2 tmpCoordinate = new Vector2(dx, dy);
+                // 실제로 생성되는 타일의 위치는 0,0을 중심으로 생성되야 하기 때문에 좌표값을 할당하지 않는다.
                 var tileObject = tileManager.Create(tileManager.transform, new Vector2(x, y), Color.white);
-                tileObject.Coordinate = new Vector2(dx, dy);
-                tileObject.name = $"Tile({tileObject.Coordinate.x}.{tileObject.Coordinate.y})";
+                tileObject.Coordinate = new Vector2(dy, dx);
+                tileObject.name = $"Tile({dx}.{dy})";
 
+                // dx, dy를 사용해 실제 타일의 위치값과 좌표값을 일치시긴다.
                 TileArray[dy, dx] = tileObject;
-                dy += 1;
+                dx += 1;
             }
-            dy = 0;
-            dx += 1;
+            dx = 0;
+            dy += 1;
         }
     }
 }
---
+++ Assets/Scripts/Map/Room.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Room
+{
+    private int x, y, w, h;
+
+    public Room(Container container)
+    {
+        this.x = container.x + Random.Range(1, container.w / 3);
+        this.y = container.y + Random.Range(1, container.h / 3);
+        this.w = container.w - (this.x - container.x);
+        this.h = container.h - (this.y - container.y);
+        this.w -= Random.Range(1, this.w / 3);
+        this.h -= Random.Range(1, this.h / 3);
+    }
+
+    public void PaintGround(Tile[,] tileArray)
+    {
+        for (int i = x; i < x + w; i++)
+        {
+            for (int j = y; j < y + h; j++)
+            {
+                tileArray[i, j].color = Color.cyan;
+                tileArray[i, j].type = Tile.Type.GROUND;
+            }
+        }
+    }
+}
