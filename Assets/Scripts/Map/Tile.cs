856e749f7ce04a2c1f73c1ee4181edca895bac99
imjae <code@kakao.com>
2022-02-19 13:24:52 +0900

    Tile 타입 틀 제작

--- Assets/Scripts/Map/Tile.cs
+++ Assets/Scripts/Map/Tile.cs
@@ -38,10 +38,39 @@ public class Tile : MonoBehaviour
     public enum Type
     {
         GROUND,
-        WALL,
-        WAY,
-        WATER,
-        LAVA
+        GROUND_LEFT_TOP,
+        GROUND_RIGHT_TOP,
+        GROUND_LEFT_BOTTOM,
+        GROUND_RIGHT_BOTTOM,
+        WALL_LEFT,
+        WALL_RIGHT,
+        WALL_TOP,
+        WALL_BOTTOM,
+        WALL_LEFT_TOP,
+        WALL_RIGHT_TOP,
+        WALL_LEFT_BOTTOM,
+        WALL_RIGHT_BOTTOM,
+        WAY_BOTTOM,
+        WAY_TOP,
+        WAY_RIGHT_TOP_START,
+        WAY_RIGHT_BOTTOM_START,
+        WAY_LEFT_TOP_START,
+        WAY_LEFT_BOTTOM_START,
+        WAY_BOTTOM_LEFT_START,
+        WAY_BOTTOM_RIGHT_START,
+        WAY_TOP_LEFT_START,
+        WAY_TOP_RIGHT_START,
+        STRUCTURE_ROUNDDOOR_LEFT,
+        STRUCTURE_ROUNDDOOR_RIGHT,
+        STRUCTURE_SQUAREDOOR_LEFT,
+        STRUCTURE_SQUAREDOOR_RIGHT,
+        STRUCTURE_VENT,
+        STRUCTURE_LADDER,
+        STRUCTURE_BIG_ROCK,
+        STRUCTURE_CEILLING,
+        STRUCTURE_FLOOR,
+        STRUCTURE_DARK,
+        STRUCTURE_SMALL_ROCK
     }
     #endregion
 
