f3901812d8f469931806678a1c2a13e8e948e5a7
ChanYoungH <96222069+ChanYongH@users.noreply.github.com>
2022-02-23 23:53:51 +0900

    PlayerUI&RandomWeapon

--- Assets/Scripts/Player/Player/Weapons/ProjectionWeapons.cs
+++ Assets/Scripts/Player/Player/Weapons/ProjectionWeapons.cs
@@ -6,16 +6,43 @@ public class ProjectionWeapons : Weapons
 {
     [SerializeField] protected float coolTime;
     [SerializeField] protected float proSpeed;
-    [SerializeField] protected float proDamage;
+    //[SerializeField] protected float proDamage;
     [SerializeField] protected float proRange;
-    [SerializeField] protected float proDuration;
+    //[SerializeField] protected float proDuration;
+
+    protected Animator weaponAni;
+    public BoxCollider2D boxCol;
+
+    private void Start()
+    {
+        boxCol = GetComponent<BoxCollider2D>();
+        weaponAni = GetComponent<Animator>();
+        //StartCoroutine(LaunchInterval());
+    }
 
     public override void LevelUp()
     {
-        throw new System.NotImplementedException(); // 개발 되지 않은 메서드에 대해 이 예외를 throw
+        throw new System.NotImplementedException(); // 구현 되지 않은 메서드에 대해 이 예외를 throw
     }
     public override void WeaponEffect()
     {
-        throw new System.NotImplementedException(); // 개발 되지 않은 메서드에 대해 이 예외를 throw
+        throw new System.NotImplementedException(); // 구현 되지 않은 메서드에 대해 이 예외를 throw
     }
+    protected IEnumerator LaunchInterval() // 발사 간격(공격 속도)
+    {
+        while (true)
+        {
+            boxCol.enabled = true;
+            weaponAni.SetTrigger("isAttack");
+            yield return new WaitForSeconds(coolTime);
+            boxCol.enabled = false;
+        }
+    }
+    protected void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Monster"))
+            other.GetComponent<Monster>().Hp -= att;
+    }
+
+
 }
--- Assets/Scripts/Player/Player/Weapons/Weapons.cs
+++ Assets/Scripts/Player/Player/Weapons/Weapons.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 
 public abstract class Weapons : MonoBehaviour
 {
-    public int att;
-    private int level;
-    private float duration;
+    public string weaponName;
+    public string weaponInfo;
+    public int level;
+    [SerializeField] protected int att; // 플레이어에서 처리할거면 protected가 나은 것 같아서 수정
+    [SerializeField] protected float duration;
+
+    public int Level
+    {
+        get { return level; }
+        set
+        {
+            level = value;
+            LevelUp();
+        }
+    }
 
     public abstract void LevelUp();
     public abstract void WeaponEffect();
