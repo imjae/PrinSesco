96c0dc859db9f946200051706638709a1f51f186
KimChanyoung <qurioturio@gmail.com>
2022-02-23 18:05:34 +0900

    Added Popup UI(ItemSelect, CharacterInfo)

--- Assets/Scripts/UI/Popup.cs
+++ Assets/Scripts/UI/Popup.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class Popup : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private GameObject popupUI;
+    
     void Start()
     {
-        
     }
 
     // Update is called once per frame
