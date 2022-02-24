dd494a8f407f4e016bd99ea963c1c499f5009dae
peewoong <fridaymorning99@naver.com>
2022-02-24 10:50:27 +0900

    [윤주] 포톤 설치_220224
    포톤 설치만! 했습니다..
    세팅은 내일(25일 금요일)에 하겠습니다.

---
+++ Assets/Scripts/Network/NetworkManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.SceneManagement;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    
+}
