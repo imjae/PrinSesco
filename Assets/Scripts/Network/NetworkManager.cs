37ff4175f185ca70e4e4cd8cf349d8201c710c83
peewoong <fridaymorning99@naver.com>
2022-03-01 16:26:42 +0900

    [윤주] 포톤 세팅_220301
    1. 포톤 세팅
    - 전체적인 흐름 연결
    
    2. 이슈 (예정)
    - 현재 방에 들어와있는 인원 수 null
    - 방 번호 랜덤 부여 > 중복 발생 가능
    - 아직 빌드해서 photonView로 연결해보진 않았음

--- Assets/Scripts/Network/NetworkManager.cs
+++ Assets/Scripts/Network/NetworkManager.cs
@@ -11,62 +11,85 @@ public class NetworkManager : MonoBehaviourPunCallbacks
 {
     string gameVersion = "1";
     public TextMeshProUGUI connectionInfoText;
-    public Button joinButton;
+    public Button connectButton;
+    public InputField nickNameField;
+    public Button nickNameButton;
 
-    // 게임 실행과 동시에 마스터 서버 접속 시도
+    public Button createRoomButton;
+    public GameObject createRoomPanel;
+    RoomInfo roomInfo;
+    public TextMeshProUGUI peopleInfoText;
+    int roomNumber;
+    public TextMeshProUGUI roomNumberText;
+
+    public Button joinRoomButton;
+    public Button startButton;
+
+    // 시작과 동시에 서버 접속 시도
     private void Start()
     {
-        PhotonNetwork.GameVersion = this.gameVersion;
+        PhotonNetwork.GameVersion = gameVersion;
         PhotonNetwork.ConnectUsingSettings();
-
-        this.joinButton.interactable = false;
-        this.connectionInfoText.text = "마스터 서버에 접속중...";
+        connectButton.interactable = false;
+        connectionInfoText.text = "마스터 서버에 접속중...";
     }
 
-    // 마스터 서버 접속 성공시 자동 실행
+    // 서버 접속 성공
     public override void OnConnectedToMaster()
     {
-        this.joinButton.interactable = true;
-        this.connectionInfoText.text = "온라인 : 마스터 서버와 연결됨";
-        Connect();
+        connectButton.interactable = true;
+        connectionInfoText.text = "온라인 : 마스터 서버와 연결됨";
+
+        nickNameField.gameObject.SetActive(true);
+        nickNameButton.gameObject.SetActive(true);
+        nickNameButton.interactable = false;
     }
 
-    // 마스터 서버와 접속 실패시 자동 실행
+    private void Update()
+    {
+        if (nickNameField.text.Length != 0) nickNameButton.interactable = true;
+    }
+
+    // 닉네임 사용 확정 -> 방 만들기
+    public void UsingNickname()
+    {
+        nickNameButton.interactable = false;
+        nickNameField.interactable = false;
+
+        createRoomButton.gameObject.SetActive(true);
+        joinRoomButton.gameObject.SetActive(true);
+    }
+
+    // 서버 접속 실패 시 재시도
     public override void OnDisconnected(DisconnectCause cause)
     {
-        this.joinButton.interactable = false;
-        this.connectionInfoText.text = "오프라인 : 마스터 서버와 연결되지 않음 \n 접속 재시도중...";
-
+        connectButton.interactable = false;
+        connectionInfoText.text = "오프라인 : 마스터 서버와 연결되지 않음 \n 접속 재시도";
         PhotonNetwork.ConnectUsingSettings();
     }
 
-    // 룸 접속 시도
-    public void Connect()
+    // 방 만들기
+    public void CreateRoom()
     {
-        this.joinButton.interactable = false;
-
-        if(PhotonNetwork.IsConnected)
-        {
-            this.connectionInfoText.text = "룸에 접속중...";
-            PhotonNetwork.JoinRandomRoom();
-        }
-
-        else
-        {
-            this.connectionInfoText.text = "오프라인 : 마스터 서버와 연결 끊김 \n 다시 접속 시도합니다.";
-            PhotonNetwork.ConnectUsingSettings();
-        }
-    }
-
-    public override void OnJoinRandomFailed(short returnCode, string message)
-    {
-        this.connectionInfoText.text = "빈 방이 없음, 새로운 방 생성...";
+        createRoomPanel.SetActive(true);
         PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = 2 });
+        startButton.interactable = false;
+
+        // 룸 넘버 랜덤으로 부여
+        roomNumber = Random.Range(1000, 9999);
+
+        if (PhotonNetwork.IsConnected)
+        {
+            roomNumberText.text = "룸 넘버 : " + roomNumber;
+            // photonView 연결을 안해서 그런지 오류 발생
+            peopleInfoText.text = roomInfo.PlayerCount + "/ " + roomInfo.MaxPlayers; 
+            if (roomInfo.PlayerCount >= 2) startButton.interactable = true;
+        }
     }
 
-    public override void OnJoinedRoom()
+    // 게임 시작 (씬 이동)
+    public void StartGame()
     {
-        this.connectionInfoText.text = "방 참가 성공!";
         PhotonNetwork.LoadLevel("NetworkMain");
     }
 }
