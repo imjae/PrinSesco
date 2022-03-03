155312c4c6e7ef8ceefc915e6c55c58db3c400b7
imjae <code@kakao.com>
2022-03-03 12:01:05 +0900

    벽 콜라이더 변수명 변경

--- Assets/Scripts/Map/TileManager.cs
+++ Assets/Scripts/Map/TileManager.cs
@@ -101,8 +101,8 @@ public class TileManager : Singleton<TileManager>
         if((tile.IsContainString("Wall") || tile.IsContainString("Entrance")) && !tile.IsContainString("Floor"))
         {
             tile.transform.SetParent(wallTileBucket);
-            var box = tile.gameObject.AddComponent<BoxCollider2D>();
-            box.usedByComposite = true;
+            var wallCollider = tile.gameObject.AddComponent<BoxCollider2D>();
+            wallCollider.usedByComposite = true;
         }
         else if(tile.IsContainString("Ground"))
         {
