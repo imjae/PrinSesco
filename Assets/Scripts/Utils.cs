8adb97129dcfeac687013dcd96c34a3be33375f2
imjae <code@kakao.com>
2022-03-06 22:49:35 +0900

    뼈 생성 로직 구조 생성

--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -2,12 +2,12 @@ using UnityEngine;
 
 public static class Utils
 {
-    // 인자가 클수록 확률이 낮아진다.
-    public static bool RandomPer(int per)
+    // 경우의 수를 인자로 받아 0이 나올 확률을 구한다.
+    public static bool RandomPer(int numberOfCase)
     {
         bool result = default(bool);
 
-        if (Random.Range(0, per) == 0) result = true;
+        if (Random.Range(0, numberOfCase) == 0) result = true;
         else result = false;
 
         return result;
