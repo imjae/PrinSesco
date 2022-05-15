93e4a12e21653173656e519094750a7350fcfd51
KimChanyoung <qurioturio@gmail.com>
2022-05-15 20:28:41 +0900

    [UI] 타이틀 완성, 메인화면으로 넘어갑니다

---
+++ Assets/Scripts/UI/flash.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class flash : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+//        TextMeshProUGUI text = gameObject.GetComponent<TextMeshPro>; ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
